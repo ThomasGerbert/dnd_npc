27e5d9ba85eb8e72eebc1caf4a9e9a560dd83c1c
Thomas Gerbert <tg55jyka@studserv.uni-leipzig.de>
2022-05-29 20:57:46 +0200

    #3: added a button to add a relationship between 2 selected nodes

---
+++ src/components/AddRelation.vue
@@ -0,0 +1,30 @@
+<template>
+    <button @click="selectOnClick">
+        Add Relationship
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'AddRelation',
+    props: ["viz"],
+
+    data() {
+         return{
+            id0: -1,
+            relation: "loves"
+        }
+    },
+    methods: {
+        async selectOnClick(){
+                this.id0 = this.viz.selectedItem
+                 this.$emit('connect', {
+                    id0: this.id0,
+                    relation: this.relation
+                })
+        },
+
+
+    } 
+}
+</script>
--- src/components/GraphCanvas.vue
+++ src/components/GraphCanvas.vue
@@ -2,6 +2,7 @@
         <div id="viz"></div>
         <AddNpc :viz="Viz"/>
         <DeleteNpc :viz="Viz"/>
+        <AddRelation :viz="Viz" @connect="prepareConnection"/>
 </template>
 
 <script>
@@ -9,16 +10,27 @@
 import NeoVis from '../neovis/neovis';
 import AddNpc from './AddNpc.vue';
 import DeleteNpc from './DeleteNpc.vue';
+import AddRelation from './AddRelation.vue';
 export default {
   name: 'GraphCanvas',
     components: {
-        AddNpc,
-        DeleteNpc
-    },
+    AddNpc,
+    DeleteNpc,
+    AddRelation
+},
   props: {
   }, 
   data() {
-      return{Viz: {}
+      return {
+          Viz: {},
+          id0: -1,
+          realation: ""
+      } 
+  },
+  methods: {
+      prepareConnection(value) {
+        this.id0 = value.id0,
+        this.realation = value.relation
       }
   },
   mounted: function(){
@@ -42,6 +54,48 @@ export default {
 
         this.Viz = new NeoVis(config);
         this.Viz.render();
+
+        this.Viz.registerOnEvent('clickNode', async (properties) => {
+        const nodeId = properties.nodeId;
+        if(this.id0 != -1){
+            //TOODO
+
+                    const neo4j = require('neo4j-driver')
+                    const uri = "neo4j://b700f583.databases.neo4j.io"
+                    const user =  "neo4j"
+                    const password = "ik92Fa_WUGsyM5t49MoAuSFz0JershP-9qNTw2TP2ig"
+
+                    const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
+                    const session = driver.session()
+
+                    try {
+                    const result = await session.run(
+                        'MATCH (a:NPC), (b:NPC) WHERE id(a) = $id0 AND id(b) = $id1 CREATE (a)-[r:'+this.realation+']->(b) RETURN type(r)',
+                        {
+                            id0: this.id0,
+                            id1 : nodeId
+                        }
+                    )
+
+                    const singleRecord = result.records[0]
+                    const node = singleRecord.get(0)
+                    console.log(node)
+
+                    } finally {
+                    await session.close()
+                    }
+                    await driver.close()
+                    this.Viz.reload()
+
+
+
+
+
+            console.log("connnect")
+            this.id0 = -1;
+        }
+    // Do something
+        });
         console.log(this.Viz);
   }
 }
